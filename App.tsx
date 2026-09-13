28cb6736114bb8b715c3f3e3de8b0b8c07b4abef
juttbadshah256-coder <juttbadshah256@gmail.com>
2026-09-13 11:56:23 -0700

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="landing-page">
+      <div className="content">
+        <h1>Paradise Nursery</h1>
+
+        <p>Welcome to Paradise Nursery</p>
+
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
